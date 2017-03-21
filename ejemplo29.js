61115aebcaf53f52c3bc22c5bd652eb8d2e7b386
Chedmoya <chedmoya@hotmail.com>
2017-03-20 19:20:05 -0500

    otro commit

---
+++ ejemplo29.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('.buttons').hover(function(){
+		$(this).addClass('hover');
+		},function(){
+			$(this).removeClass('hover');
+		}
+	);
+	$('#boldbutton').bind('click', function(){
+		$('#info').removeClass('italicmatter');
+		$('#info').addClass('boldmatter');
+	});
+	$('#italicbutton').bind('click', function(){
+		$('#info').removeClass('boldmatter');
+		$('#info').addClass('italicmatter');
+	});
+});
